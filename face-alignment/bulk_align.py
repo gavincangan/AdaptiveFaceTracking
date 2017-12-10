86d549412f81bb3e9e91e21b941e18646065c97f
ilya7456 <ipozdneev@gmail.com>
2017-12-10 17:51:58 -0500

    starting to build CNN dataset

--- face-alignment/bulk_align.py
+++ face-alignment/bulk_align.py
@@ -14,14 +14,15 @@ import os
 from os import listdir
 from os.path import isfile, join
 
-def align_image(image, fa, imname="img"):
+def align_image(image, fa, imname="img", dest="./results/"):
     # load the input image, resize it, and convert it to grayscale
+    print('Processing', imname)
     image = imutils.resize(image, width=800)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # show the original input image and detect faces in the grayscale
     # image
-    cv2.imshow("Input", image)
+    # cv2.imshow("Input", image)
     rects = detector(gray, 2)
 
     # loop over the face detections
@@ -29,15 +30,23 @@ def align_image(image, fa, imname="img"):
         # extract the ROI of the *original* face, then align the face
         # using facial landmarks
         (x, y, w, h) = rect_to_bb(rect)
-        faceOrig = imutils.resize(image[y:y + h, x:x + w], width=256)
-        faceAligned = fa.align(image, gray, rect)
+        if (w < 10 or h < 0):
+            print('Skipping', imname)
+            continue
+        try:
+            faceOrig = imutils.resize(image[y:y + h, x:x + w], width=256)
+            faceAligned = fa.align(image, gray, rect)
+        except:
+            print('Skipping', imname)
+            continue
 
         # import uuid
         # f = str(uuid.uuid4())
-        path = "./results/" + os.path.splitext(imname)[0] + "/" # + str(idx) + ".png"
+        # path = "./results/" + os.path.splitext(imname)[0] + "/" # + str(idx) + ".png"
+        path = dest
         if not os.path.exists(path):
                 os.makedirs(path)
-        path = path + str(idx) + ".png"
+        path = path + os.path.splitext(imname)[0] + "_" + str(idx) + ".png"
         print("Saving", path)
         cv2.imwrite(path, faceAligned)
 
@@ -53,8 +62,10 @@ ap.add_argument("-p", "--shape-predictor", required=True,
 	help="path to facial landmark predictor")
 #ap.add_argument("-i", "--image", required=True,
 #	help="path to input image")
-ap.add_argument("-d", "--imdir", required=True,
+ap.add_argument("-i", "--imdir", required=True,
 	help="path to input image")
+ap.add_argument("-d", "--dest", required=True,
+	help="path to save image")
 args = vars(ap.parse_args())
 
 # initialize dlib's face detector (HOG-based) and then create
@@ -63,10 +74,13 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(args["shape_predictor"])
 fa = FaceAligner(predictor, desiredFaceWidth=256)
 mydir = args["imdir"]
+dest = args["dest"]
 onlyimages = [f for f in listdir(mydir) if isfile(join(mydir, f))]
 for imfile in onlyimages:
     if mydir[-1] != '/':
         mydir += '/'
+    if dest[-1] != '/':
+        dest += '/'
     image = cv2.imread(mydir+imfile)
-    align_image(image, fa, imfile)
+    align_image(image, fa, imfile, dest)
 #cv2.waitKey(0)
--- face-alignment/bulk_run.sh
+++ face-alignment/bulk_run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 echo "Make sure the --imdir points to the source image directory"
-python ./bulk_align.py --shape-predictor shape_predictor_68_face_landmarks.dat --imdir ../images/
+python ./bulk_align.py --shape-predictor shape_predictor_68_face_landmarks.dat --imdir ../raw_bbt/sheldon/ --dest ../labeled_bbt/sheldon/

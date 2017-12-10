8f3dddccd4b8e887a72b000e982ab905da27c4df
ilya7456 <ipozdneev@gmail.com>
2017-12-10 18:08:18 -0500

    leonard done

--- face-alignment/bulk_run.sh
+++ face-alignment/bulk_run.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 echo "Make sure the --imdir points to the source image directory"
-python ./bulk_align.py --shape-predictor shape_predictor_68_face_landmarks.dat --imdir ../raw_bbt/sheldon/ --dest ../labeled_bbt/sheldon/
+#python ./bulk_align.py --shape-predictor shape_predictor_68_face_landmarks.dat --imdir ../raw_bbt/sheldon/ --dest ../labeled_bbt/sheldon/
+python ./bulk_align.py --shape-predictor shape_predictor_68_face_landmarks.dat --imdir ../raw_bbt/leonard/ --dest ../labeled_bbt/leonard/

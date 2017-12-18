e7d8ba935f3761b07a546f35b90320804e434e46
ilya7456 <ipozdneev@gmail.com>
2017-12-17 20:42:11 -0500

    update

--- tf/tensorflow-for-poets-2/run.sh
+++ tf/tensorflow-for-poets-2/run.sh
@@ -13,11 +13,12 @@
 
 python -m scripts.retrain \
 	--bottleneck_dir=tf_files/bottlenecks \
-	--how_many_training_steps=1000 \
+	--how_many_training_steps=4000 \
 	--model_dir=tf_files/models/ \
 	--summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
 	--output_graph=tf_files/retrained_graph.pb \
 	--output_labels=tf_files/retrained_labels.txt \
 	--architecture="${ARCHITECTURE}" \
-	--image_dir=../../labeled_bbt
-
+	--image_dir=../../labeled_bbt \
+	--learning_rate=0.001 \
+	--print_misclassified_test_images
--- tf/tensorflow-for-poets-2/settings.sh
+++ tf/tensorflow-for-poets-2/settings.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-echo 'Run the following commands'
-echo 'export IMAGE_SIZE=224'
-echo 'export ARCHITECTURE="mobilenet_0.50_${IMAGE_SIZE}"'
+export IMAGE_SIZE=224
+export ARCHITECTURE="mobilenet_0.50_${IMAGE_SIZE}"

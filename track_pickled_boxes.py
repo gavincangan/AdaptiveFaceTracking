e3aaef593feea3db1ecabaae9a5d35f4d328d1d3
Barnabas Gavin Cangan <gavincangan@gmail.com>
2017-12-18 12:13:22 -0500

    update tracker.

--- track_pickled_boxes.py
+++ track_pickled_boxes.py
@@ -34,29 +34,14 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 bottomLeftCornerOfText = (10,500)
 fontScale = 1
 lineType = 2
-personColors = ((128, 0, 0),
-        (0, 128, 0),
-        (128, 128, 0),
-        (0, 0, 128),
-        (128, 0, 128),
-        (0, 128, 128),
-        (128, 128, 128),
-        (255, 0, 0),
-        (0, 255, 0),
-        (255, 255, 0),
-        (0, 0, 255),
-        (255, 0, 255))
+personColors = ((255, 0, 0), (0, 255, 0), (0, 0, 255))
 
 INVALID = -1
 LEONARD = 1
 PENNY = 2
 SHELDON = 3
-HOWARD = 4
-RAJ = 5
-BERNADETTE = 6
-AMY = 7
 
-faces = [LEONARD, PENNY, SHELDON, HOWARD, RAJ, BERNADETTE, AMY]
+faces = [SHELDON, LEONARD, PENNY]
 face_tracker = dict()
 
 start_frame_num = 80
@@ -64,49 +49,28 @@ end_frame_num = 24499
 
 
 def get_name_string(person_id):
-    if person_id == LEONARD:
-        return "leonard"
-    elif person_id == PENNY:
-        return "penny"
-    elif person_id == SHELDON:
-        return "sheldon"
-    elif person_id == HOWARD:
-        return "howard"
-    elif person_id == RAJ:
-        return "raj"
-    elif person_id == BERNADETTE:
-        return "bernadette"
-    elif person_id == AMY:
-        return "amy"
-    elif person_id == INVALID:
-        return "invalid"
-    else:
-        raise ValueError('person_id not found')
-
-def get_name_id(person_name):
-    if person_name == "leonard":
-        return LEONARD
-    elif person_name == "penny":
-        return PENNY
-    elif person_name == "sheldon":
-        return SHELDON
-    elif person_name == "howard":
-        return HOWARD
-    elif person_name == "raj":
-        return RAJ
-    elif person_name == "bernadette":
-        return BERNADETTE
-    elif person_name == "amy":
-        return AMY
-    elif person_name == "invalid":
-        return INVALID
-    else:
-        raise ValueError('person_name not found')
+    if person_id == 1:
+        return "Leonard"
+    elif person_id == 2:
+        return "Penny"
+    elif person_id == 3:
+        return "Sheldon"
 
 def get_face_in_box(this_frame, box):
     # print 'Get face in box ', box
     return this_frame[box[1]:box[1] + box[3], box[0]:box[0] + box[2], :]
 
+def calc_box_distance(box1, box2):
+    return (box2[0] - box1[0])**2 + (box2[1] - box1[1])**2 + (box2[2] - box1[2])**2 + (box2[3] - box1[3])**2
+
+def is_box_listed_already(boxes_in_frame, new_box):
+    ret_val = False
+    dist_threshold = 100
+    for this_box in boxes_in_frame:
+        box_dist = calc_box_distance(this_box, new_box)
+        if(box_dist < dist_threshold):
+            ret_val = True
+    return ret_val
 
 def get_pts_in_box(box):
     return (box[0], box[1]), (box[0] + box[2], box[1] + box[3])
@@ -114,8 +78,14 @@ def get_pts_in_box(box):
 
 def recognize_person(face_image):
     (label, score) = cnn_classifier.run_data(face_image)
-    result = get_name_id(label)
-    # print 'Face identified: ', label, score, result
+    result = INVALID
+    if label == "sheldon":
+        result = SHELDON
+    elif label == "leonard":
+        result = LEONARD
+    elif label == "penny":
+        result = PENNY
+    print 'Face identified: ', label, score, result
     return result
 
 def get_frame(frame_num):
@@ -183,8 +153,43 @@ def track_faces(frame_limit=-1):
 
                     # # No need to call face recognition every time.
                     # # Only if tracker fails
-                    tracked_next_frame_box[(next_frame_num, this_person)] = next_box
-                    person_in_frame_box[(next_frame_num, next_box)] = this_person
+                    add_new_box = False
+                    next_frame_boxes = []
+                    if next_frame_num in box_in_frame.keys():
+                        next_frame_boxes = box_in_frame[next_frame_num]
+                    if (next_frame_num, this_person) not in tracked_next_frame_box.keys():
+                        if not is_box_listed_already(next_frame_boxes, next_box):
+                            add_new_box = True
+                            next_person = this_person
+                        else:
+                            continue
+                    else:
+                        if not is_box_listed_already(next_frame_boxes, next_box):
+                            next_face = get_face_in_box(next_frame_img, next_box)
+                            next_person = recognize_person(next_face)
+                            if next_person is not INVALID:
+                                add_new_box = True
+                            else:
+                                continue
+                        else:
+                            continue
+                    if add_new_box:
+                        tracked_next_frame_box[(next_frame_num, this_person)] = next_box
+                        person_in_frame_box[(next_frame_num, next_box)] = next_person
+
+                        if next_frame_num not in box_in_frame.keys():
+                            boxes_in_next_frame = list()
+                            boxes_in_next_frame.append(next_box)
+                            boxes_in_next_frame = tuple(boxes_in_next_frame)
+                            box_in_frame[next_frame_num] = boxes_in_next_frame
+                            print 'Added box: ', next_box, 'in frame :', next_frame_num
+
+                        if not is_box_listed_already(box_in_frame[next_frame_num], next_box):
+                            boxes_in_next_frame = list(box_in_frame[next_frame_num])
+                            boxes_in_next_frame.append(next_box)
+                            boxes_in_next_frame = tuple(boxes_in_next_frame)
+                            box_in_frame[next_frame_num] = boxes_in_next_frame
+                            print 'Added new box: ', next_box, 'in frame :', next_frame_num
                 else:
                     continue
             else:
@@ -201,7 +206,9 @@ def track_faces(frame_limit=-1):
     print "Saving data"
     pickle.dump(person_in_frame_box, person_in_frame_box_fp)
     pickle.dump(tracked_next_frame_box, tracked_next_frame_box_fp)
+    pickle.dump(box_in_frame, box_in_frame_fp)
 
+    box_in_frame_fp.close()
     person_in_frame_box_fp.close()
     tracked_next_frame_box_fp.close()
 
@@ -222,6 +229,24 @@ def imwrite_output(frame_limit=-1):
                 cv2.rectangle(this_frame_img, this_pt1, this_pt2, personColors[this_person - 1])
                 cv2.putText(this_frame_img, get_name_string(this_person), this_pt1, font, fontScale, personColors[this_person - 1], lineType)
 
+        frame_filename = output_frames_folder + '/' + "%05d.jpg" % this_frame_num
+        cv2.imwrite(frame_filename, this_frame_img)
+        print ''
+
+
+def draw_all_boxes(frame_limit=-1):
+    global end_frame_num
+    if frame_limit is not -1:
+        end_frame_num = start_frame_num + frame_limit
+
+    for this_frame_num in range(start_frame_num, end_frame_num):
+        print this_frame_num,
+        this_frame_img = get_frame(this_frame_num)
+        if this_frame_num in box_in_frame.keys():
+            this_frame_boxes = box_in_frame[this_frame_num]
+            for this_box in this_frame_boxes:
+                [this_pt1, this_pt2] = get_pts_in_box(this_box)
+                cv2.rectangle(this_frame_img, this_pt1, this_pt2, personColors[0])
         frame_filename = output_frames_folder + '/' + "%05d.jpg" % this_frame_num
         cv2.imwrite(frame_filename, this_frame_img)
         print ''
@@ -252,6 +277,6 @@ def collect_boxes_in_frames():
 
 if __name__ == '__main__':
     # collect_boxes_in_frames()
-    track_faces(100)
-    imwrite_output(100)
-
+    track_faces(500)
+    imwrite_output(500)
+    # draw_all_boxes(10)

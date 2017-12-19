b36d44c3de6785fc2aa44d458f5e4bb6c0ca92fc
ilya7456 <ipozdneev@gmail.com>
2017-12-18 22:14:46 -0500

    retrained cnn

--- tf/tensorflow-for-poets-2/run.sh
+++ tf/tensorflow-for-poets-2/run.sh
@@ -13,12 +13,12 @@
 
 python -m scripts.retrain \
 	--bottleneck_dir=tf_files/bottlenecks \
-	--how_many_training_steps=2000 \
+	--how_many_training_steps=10000 \
 	--model_dir=tf_files/models/ \
 	--summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
 	--output_graph=tf_files/retrained_graph.pb \
 	--output_labels=tf_files/retrained_labels.txt \
 	--architecture="${ARCHITECTURE}" \
 	--image_dir=../../labeled_bbt \
-	--learning_rate=0.001 \
+	--learning_rate=0.0005 \
 	--print_misclassified_test_images
